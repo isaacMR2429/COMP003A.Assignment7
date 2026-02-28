e89ec7f9ec2be6a46f0a005c5ab77ccb3c770739
Isaac Martinez <isaacmartinez2429@gmail.com>
2026-02-27 22:06:12 -0800

    just made the menu-driven program with the required options and looping it

--- COMP003A.Assignment7/Program.cs
+++ COMP003A.Assignment7/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             List<int> scores = new List<int> { 85, 90, 78, 92, 88 };
+
+            bool working = true;
+            while (working)
+            {
+                Console.WriteLine("Welcome to the Student Score Analyzer");
+                Console.WriteLine("1. Display The Scores");
+                Console.WriteLine("2. Show their Total");
+                Console.WriteLine("3. Show their Average");
+                Console.WriteLine("4. Exit Program");
+                string input = Console.ReadLine();
+            }
         }
     }
 }
